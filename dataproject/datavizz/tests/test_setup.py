a697789642e6d746afcd67e80fc854f5b0be2cb2
Akolade <emmakolade@gmail.com>
2023-02-04 04:33:26 +0100

    unit testing

--- dataproject/datavizz/tests/test_setup.py
+++ dataproject/datavizz/tests/test_setup.py
@@ -1,16 +1,18 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase
+from ..models import DataFile
 
 
 class TestSetup(APITestCase):
     def setUp(self):
         self.register_url = reverse('register')
         self.login_url = reverse('login')
+        self.upload_url = reverse('upload')
+        self.queryset = DataFile.objects.all()
 
         self.user_data = {
             'username': 'Kolade',
             'password': 'password123#',
-            # 'full_name': 'KoladeOlanipekun',
             'email': 'kolade@gmail.com',
 
         }
--- dataproject/datavizz/tests/test_views.py
+++ dataproject/datavizz/tests/test_views.py
@@ -1,13 +1,15 @@
 from .test_setup import TestSetup
 from rest_framework import status
 from ..models import User
+from ..serializers import DataFileSerializer
+from ..models import DataFile
 
 
-class TestViews(TestSetup):
+class TestView(TestSetup):
 
     def test_user_must_register_with_data(self):
         '''
-        ensures user cannot create account with no data provided 
+        ensures user cannot create account with no data provided
         '''
         response = self.client.post(self.register_url)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
@@ -35,7 +37,45 @@ class TestViews(TestSetup):
             self.login_url, self.user_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_if_user_has_the_right_login_credential(self):
+    def auth(self):
+        self.client.post(
+            self.register_url, self.user_data)
         response = self.client.post(
-            self.login_url, self.user_data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+            self.login_url, self.user_data)
+        access_token = response.data['access']
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer{access_token}")
+
+    def test_should_not_allow_upload_for_user_with_no_auth(self):
+        data = {'file': 'fakefile.csv'}
+        res = self.client.post(
+            self.upload_url, data, format='json')
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_should_not_allow_upload_for_user_with_no_auth(self):
+        self.auth()
+        data = {'file': 'fakefile.csv'}
+        res = self.client.post(
+            self.upload_url, data, format='json')
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    # def test_should_allow_auth_user_upload_file(self):
+        
+    #     self.auth()
+    #     data = {'file': 'fakefile.csv'}
+    #     res = self.client.post(
+    #         self.upload_url, data, format='json')
+    #     # serializer = DataFileSerializer(data)
+    #     # self.assertTrue(serializer.is_valid())
+    #     # self.perform_create(serializer)
+    #     self.assertEqual(DataFile.objects.count(), 1)
+    #     self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+
+# class TestViewsData(TestSetupData):
+
+#     def test_should_not_allow_upload_for_user_with_no_auth(self):
+#         data = {'file': 'fakefile.csv'}
+#         res = self.client.post(
+#             self.upload_url, data, format='json')
+#         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+        
+        # self.assertRaises(serializer.ValidationError)
